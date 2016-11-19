906c7961e096de82813ff07ce9215b5dafad746c
lj614418910 <lijia@riyoshimitekiMacBook-Pro.local>
2016-11-19 22:54:58 +0800

    第一次改动，基本分区

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,7 +25,6 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue',
       'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
       'components': path.resolve(__dirname, '../src/components')
     }
   },
--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
 
 /* eslint-disable no-new */
-new Vue({
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// });
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
+const router = new VueRouter({
+  routes
+});
+const app = new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
-})
+  components: { App },
+  router
+}).$mount('#app');
+router.push('/goods');
