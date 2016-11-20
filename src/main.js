899294a841b5b9174cf10b2ff5ce1067c5b3ab73
lj614418910 <lijia@riyoshimitekiMacBook-Pro.local>
2016-11-20 16:35:25 +0800

    header组件初步写成

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 import goods from './components/goods/goods';
 import ratings from './components/ratings/ratings';
 import seller from './components/seller/seller';
 
-/* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   template: '<App/>',
-//   components: { App }
-// });
+import 'common/stylus/index.styl';
+
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const routes = [
   { path: '/goods', component: goods },
@@ -19,7 +17,8 @@ const routes = [
   { path: '/seller', component: seller }
 ];
 const router = new VueRouter({
-  routes
+  routes,
+  linkActiveClass: 'active'
 });
 const app = new Vue({
   el: '#app',
