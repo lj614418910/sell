aee6cf8c3938047a063ba1c342a9430535194ced
lijia_i <lijia_i@didichuxing.com>
2017-03-24 10:04:46 +0800

    react-router

--- src/main.js
+++ src/main.js
@@ -2,31 +2,37 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import App from './App';
-import goods from './components/goods/goods';
-import ratings from './components/ratings/ratings';
-import seller from './components/seller/seller';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
 
 import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-const routes = [
-  { path: '/goods', component: goods },
-  { path: '/ratings', component: ratings },
-  { path: '/seller', component: seller }
-];
+const routes = [{
+  path: '/',
+  component: goods
+}, {
+  path: '/goods',
+  component: goods
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}];
+
 const router = new VueRouter({
-  routes,
-  linkActiveClass: 'active'
+  linkActiveClass: 'active',
+  routes
 });
-const app = new Vue({
+
+/* eslint-disable no-new */
+new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App },
   router,
-  data: {
-   eventHub: new Vue()
-  }
-}).$mount('#app');
-router.push('/goods');
+  render: h => h(App)
+});
